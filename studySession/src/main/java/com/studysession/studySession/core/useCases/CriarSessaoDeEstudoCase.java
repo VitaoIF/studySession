4e3e5962e2dd728855eaff4cfef209075add342d
VitaoIF <victormelo1212@gmail.com>
2026-04-11 14:32:05 -0300

    feat(core): adiciona interface para criar sessao de estudos

--- studySession/src/main/java/com/studysession/studySession/core/useCases/CriarSessaoDeEstudoCase.java
+++ studySession/src/main/java/com/studysession/studySession/core/useCases/CriarSessaoDeEstudoCase.java
@@ -1,4 +1,8 @@
 package com.studysession.studySession.core.useCases;
 
+import com.studysession.studySession.core.entities.StudySession;
+
 public interface CriarSessaoDeEstudoCase {
+
+    public StudySession execute(StudySession session);
 }
